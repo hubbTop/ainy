fe8c7d079e942b753f749362d77c8f75aff8b64e
hubingbing <329313780@qq.com>
2018-04-27 08:45:57 +0800

    Signed-off-by: hubingbing <329313780@qq.com>

--- cs_admin4/src/com/hhdb/csadmin/common/base/IEventRoute.java
+++
@@ -1,8 +0,0 @@
-package com.hhdb.csadmin.common.base;
-
-import com.hhdb.csadmin.common.event.HHEvent;
-
-public interface IEventRoute {
-	boolean addPlugin(String  id);
-	HHEvent processEvent(HHEvent event);
-}
--- cs_admin4/src/com/hhdb/csadmin/common/base/PluginType.java
+++
@@ -1,6 +0,0 @@
-package com.hhdb.csadmin.common.base;
-
-
-public enum PluginType {
-	NONE,TREE,MENU,TOOLBAR,CONTENT,STATUS,OUTPUT;
-}
